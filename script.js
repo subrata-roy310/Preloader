92a5228e56eaaaa4f71d65cfc3e99aa261b6207e
subrata410 <subrata.rooy2022@gmail.com>
2023-01-24 15:57:54 +0600

    Website Preloader

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    const preloaderArea = document.getElementById("preloader-area");
+    preloaderArea.className = "page-loaded"
+
+    setTimeout(function () {
+        preloaderArea.style.display = "none"
+    }, 500);
+};
